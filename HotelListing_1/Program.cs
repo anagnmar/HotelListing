5aefcab27cfd176fcdf6f961bc8629a502ad0543
Margn <Margn@DESKTOP-CRCJL96>
2021-08-22 19:17:07 +0300

    Add project files.

---
+++ HotelListing_1/Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
+
+
+namespace HotelListing_1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.File(
+					path: @"C:\Users\Margn\Documents\log-.txt",
+					outputTemplate: "{Timestamp:dd-MM-yyyy HH:mm:ss} [{level:u3}]} " +
+											"{Message:lj}{NewLine}{Exception}",
+					rollingInterval: RollingInterval.Day,
+					restrictedToMinimumLevel: LogEventLevel.Information
+				)
+				.CreateLogger();
+
+			try
+			{
+				Log.Information("Application is starting...");
+
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+
+				Log.Fatal(ex, "Application failed to start.");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+
+				.UseSerilog()
+
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
